fd6038690a77d68614a0f16a5295161385b62940
Miguel Souza <souzzamiguel09@gmail.com>
2026-09-16 16:44:37 -0300

    Getting author details

--- src/main/java/com/miguelsouza/libraryapi/controller/AutorController.java
+++ src/main/java/com/miguelsouza/libraryapi/controller/AutorController.java
@@ -4,15 +4,13 @@ package com.miguelsouza.libraryapi.controller;
 import com.miguelsouza.libraryapi.controller.dto.AutorDTO;
 import com.miguelsouza.libraryapi.model.Autor;
 import com.miguelsouza.libraryapi.service.AutorService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/autores")
@@ -37,5 +35,20 @@ public class AutorController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
+
+        var idAutor = UUID.fromString(id);
+
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if (autorOptional.isPresent()) {
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(autor.getId(), autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/miguelsouza/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/com/miguelsouza/libraryapi/controller/dto/AutorDTO.java
@@ -3,8 +3,9 @@ package com.miguelsouza.libraryapi.controller.dto;
 import com.miguelsouza.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record AutorDTO(String nome, LocalDate dataNascimento, String nacionalidade) {
+public record AutorDTO(UUID id, String nome, LocalDate dataNascimento, String nacionalidade) {
 
 
     public Autor mapearParaAutor() {
