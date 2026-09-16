1989a8ffacda250b5690c8e73fd3576f60e18878
Miguel Souza <souzzamiguel09@gmail.com>
2026-09-16 16:17:49 -0300

    Adding JPA Auditing to entities

--- src/main/java/com/miguelsouza/libraryapi/Application.java
+++ src/main/java/com/miguelsouza/libraryapi/Application.java
@@ -3,10 +3,10 @@ package com.miguelsouza.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
