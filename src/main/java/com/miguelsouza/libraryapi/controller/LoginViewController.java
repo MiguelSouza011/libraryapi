8189d93b2dda21f16c03ea83c9f98b2d8fc4cce5
Miguel Souza <souzzamiguel09@gmail.com>
2026-09-25 09:56:19 -0300

    Enabling and customizing the login form

---
+++ src/main/java/com/miguelsouza/libraryapi/controller/LoginViewController.java
@@ -0,0 +1,13 @@
+package com.miguelsouza.libraryapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping
+    public String paginaLogin() {
+        return "login";
+    }
+}
